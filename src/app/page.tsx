4a40018f246068dbea400615f999df952e427dc2
AshmitSrivastava <ashmitsrivastava1607@gmail.com>
2025-12-01 15:54:32 +0530

    Projects and Journey

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,18 @@ import Hero from "@/components/sections/Hero"
 import Services from "@/components/sections/Services"
 import WorkProcess from "@/components/sections/WorkProcess"
 import ClientLogos from "@/components/sections/ClientLogos"
+import Projects from "@/components/sections/Projects"
 import Testimonials from "@/components/sections/Testimonials"
 import CTA from "@/components/sections/CTA"
+import JourneyTimeline from "@/components/sections/JourneyTimeline"
 
 export default function Home() {
   return (
     <main>
+      
       <Hero />
-      <Services />
-      <WorkProcess />
-      <ClientLogos />
-      <Testimonials />
+      <JourneyTimeline/>
+      <Projects />
       <CTA />
     </main>
   )
